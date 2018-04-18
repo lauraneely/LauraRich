90b87f8c49a1ff86287f447ee126929e56f79eca
lneely <lneely@deloitte.co.uk>
2018-04-18 11:21:00 -0400

    Colours

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -17,4 +17,8 @@ it 'displays the company name: Prototype Inc' do
   get '/'
   expect(last_response.body).to include "Prototype Inc."
 end
+it 'displays text within jumbotron' do
+  get '/'
+  expect(last_response.body).to include "A prototype is an early sample, model, or release of a product built to test a concept"
+end
 end
